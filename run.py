fc61d5111f40ada05d081431e5fbde2dc4dd7f5f
Michael Rans <rans@email.com>
2017-07-19 13:58:28 +0200

    Update test framework

--- run.py
+++ run.py
@@ -25,11 +25,10 @@ def main():
     logger.info('Number of datasets to upload: %d' % len(countriesdata))
     for countrydata in countriesdata:
         dataset, showcase = generate_dataset_and_showcase(downloader, countrydata)
-        if dataset is not None:
-            dataset.update_from_yaml()
-            dataset.create_in_hdx()
-            showcase.create_in_hdx()
-            showcase.add_dataset(dataset)
+        dataset.update_from_yaml()
+        dataset.create_in_hdx()
+        showcase.create_in_hdx()
+        showcase.add_dataset(dataset)
 
 if __name__ == '__main__':
     facade(main, hdx_site='feature', project_config_yaml=join('config', 'project_configuration.yml'))
