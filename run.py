a81af422565b91a52ce621f2a322363f99295a89
mcarans <rans@email.com>
2025-03-13 11:04:01 +1300

    Revert Dockerfile change Remove adding src to path

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 import runpy
-import sys
-from os import getcwd
-from os.path import join
 
 # Execute a module by its full module name
 runpy.run_module("hdx.scraper.worldpop", run_name="__main__")
