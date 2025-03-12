3fd641b5a116744e0bcb3f85844a47e4a209e094
mcarans <rans@email.com>
2025-03-13 11:18:09 +1300

    Remove hard coded site

--- src/hdx/scraper/worldpop/__main__.py
+++ src/hdx/scraper/worldpop/__main__.py
@@ -87,7 +87,6 @@ def main(
 if __name__ == "__main__":
     facade(
         main,
-        hdx_site="stage",
         user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
         user_agent_lookup=lookup,
         project_config_yaml=script_dir_plus_file(
